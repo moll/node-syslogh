fd446e5cde7e0d183b3c944eb2e60d263b7fa0eb
Andri Möll <andri@dot.ee>
2015-09-10 21:14:36 +0300

    Refactor ToLocalChecked call to macro.

--- index.cpp
+++ index.cpp
@@ -3,7 +3,7 @@
 #include <nan.h>
 #include <syslog.h>
 
-#define String(str) Nan::New<v8::String>(str)
+#define String(str) Nan::New<v8::String>(str).ToLocalChecked()
 #define Integer(str) Nan::New<v8::Integer>(str)
 
 // Syslog on Linux does not seem to copy the identity string itself to
@@ -34,50 +34,50 @@ namespace Syslogh {
 		Nan::SetMethod(exports, "syslog", syslog);
 		Nan::SetMethod(exports, "closelog", closelog);
 
-		exports->Set(String("CONS").ToLocalChecked(), Integer(LOG_CONS));
-		exports->Set(String("ODELAY").ToLocalChecked(), Integer(LOG_ODELAY));
-		exports->Set(String("PID").ToLocalChecked(), Integer(LOG_PID));
-		exports->Set(String("NDELAY").ToLocalChecked(), Integer(LOG_NDELAY));
-		exports->Set(String("NOWAIT").ToLocalChecked(), Integer(LOG_NOWAIT));
+		exports->Set(String("CONS"), Integer(LOG_CONS));
+		exports->Set(String("ODELAY"), Integer(LOG_ODELAY));
+		exports->Set(String("PID"), Integer(LOG_PID));
+		exports->Set(String("NDELAY"), Integer(LOG_NDELAY));
+		exports->Set(String("NOWAIT"), Integer(LOG_NOWAIT));
 
 		// Seems available on OS X but not in the UNIX syslog.h.
 		#ifdef LOG_PERROR
-		exports->Set(String("PERROR").ToLocalChecked(), Integer(LOG_PERROR));
+		exports->Set(String("PERROR"), Integer(LOG_PERROR));
 		#endif
 
-		exports->Set(String("KERN").ToLocalChecked(), Integer(LOG_KERN));
-		exports->Set(String("USER").ToLocalChecked(), Integer(LOG_USER));
-		exports->Set(String("MAIL").ToLocalChecked(), Integer(LOG_MAIL));
-		exports->Set(String("DAEMON").ToLocalChecked(), Integer(LOG_DAEMON));
-		exports->Set(String("AUTH").ToLocalChecked(), Integer(LOG_AUTH));
-		exports->Set(String("SYSLOG").ToLocalChecked(), Integer(LOG_SYSLOG));
-		exports->Set(String("LPR").ToLocalChecked(), Integer(LOG_LPR));
-		exports->Set(String("NEWS").ToLocalChecked(), Integer(LOG_NEWS));
-		exports->Set(String("UUCP").ToLocalChecked(), Integer(LOG_UUCP));
-		exports->Set(String("CRON").ToLocalChecked(), Integer(LOG_CRON));
+		exports->Set(String("KERN"), Integer(LOG_KERN));
+		exports->Set(String("USER"), Integer(LOG_USER));
+		exports->Set(String("MAIL"), Integer(LOG_MAIL));
+		exports->Set(String("DAEMON"), Integer(LOG_DAEMON));
+		exports->Set(String("AUTH"), Integer(LOG_AUTH));
+		exports->Set(String("SYSLOG"), Integer(LOG_SYSLOG));
+		exports->Set(String("LPR"), Integer(LOG_LPR));
+		exports->Set(String("NEWS"), Integer(LOG_NEWS));
+		exports->Set(String("UUCP"), Integer(LOG_UUCP));
+		exports->Set(String("CRON"), Integer(LOG_CRON));
 
 		// Available on OS X but not in the UNIX syslog.h.
 		#ifdef LOG_AUTHPRIV
-		exports->Set(String("AUTHPRIV").ToLocalChecked(), Integer(LOG_AUTHPRIV));
+		exports->Set(String("AUTHPRIV"), Integer(LOG_AUTHPRIV));
 		#endif
 
-		exports->Set(String("LOCAL0").ToLocalChecked(), Integer(LOG_LOCAL0));
-		exports->Set(String("LOCAL1").ToLocalChecked(), Integer(LOG_LOCAL1));
-		exports->Set(String("LOCAL2").ToLocalChecked(), Integer(LOG_LOCAL2));
-		exports->Set(String("LOCAL3").ToLocalChecked(), Integer(LOG_LOCAL3));
-		exports->Set(String("LOCAL4").ToLocalChecked(), Integer(LOG_LOCAL4));
-		exports->Set(String("LOCAL5").ToLocalChecked(), Integer(LOG_LOCAL5));
-		exports->Set(String("LOCAL6").ToLocalChecked(), Integer(LOG_LOCAL6));
-		exports->Set(String("LOCAL7").ToLocalChecked(), Integer(LOG_LOCAL7));
+		exports->Set(String("LOCAL0"), Integer(LOG_LOCAL0));
+		exports->Set(String("LOCAL1"), Integer(LOG_LOCAL1));
+		exports->Set(String("LOCAL2"), Integer(LOG_LOCAL2));
+		exports->Set(String("LOCAL3"), Integer(LOG_LOCAL3));
+		exports->Set(String("LOCAL4"), Integer(LOG_LOCAL4));
+		exports->Set(String("LOCAL5"), Integer(LOG_LOCAL5));
+		exports->Set(String("LOCAL6"), Integer(LOG_LOCAL6));
+		exports->Set(String("LOCAL7"), Integer(LOG_LOCAL7));
 
-		exports->Set(String("EMERG").ToLocalChecked(), Integer(LOG_EMERG));
-		exports->Set(String("ALERT").ToLocalChecked(), Integer(LOG_ALERT));
-		exports->Set(String("CRIT").ToLocalChecked(), Integer(LOG_CRIT));
-		exports->Set(String("ERR").ToLocalChecked(), Integer(LOG_ERR));
-		exports->Set(String("WARNING").ToLocalChecked(), Integer(LOG_WARNING));
-		exports->Set(String("NOTICE").ToLocalChecked(), Integer(LOG_NOTICE));
-		exports->Set(String("INFO").ToLocalChecked(), Integer(LOG_INFO));
-		exports->Set(String("DEBUG").ToLocalChecked(), Integer(LOG_DEBUG));
+		exports->Set(String("EMERG"), Integer(LOG_EMERG));
+		exports->Set(String("ALERT"), Integer(LOG_ALERT));
+		exports->Set(String("CRIT"), Integer(LOG_CRIT));
+		exports->Set(String("ERR"), Integer(LOG_ERR));
+		exports->Set(String("WARNING"), Integer(LOG_WARNING));
+		exports->Set(String("NOTICE"), Integer(LOG_NOTICE));
+		exports->Set(String("INFO"), Integer(LOG_INFO));
+		exports->Set(String("DEBUG"), Integer(LOG_DEBUG));
 	}
 }
 
